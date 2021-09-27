370a7994380d6801e5ec84eed934641fafb8ee6f
sanjanadehariya123 <91261899+sanjanadehariya123@users.noreply.github.com>
2021-09-27 03:01:19 -0700

    Update main1.py

--- CheapestAirTicket/main1.py
+++ CheapestAirTicket/main1.py
@@ -1,65 +1,81 @@
 from selectorlib import Extractor
 import requests 
 from time import sleep
-import csv
-import os
 from datetime import datetime
 from datetime import date
+import os
+import csv
 
-# Create an Extractor by reading from the YAML file
+
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-e = Extractor.from_yaml_file(__location__+'/keys.yml')
 
+e = Extractor.from_yaml_file(__location__+'/keys.yml')
 print (e)
 
 def inputDate(inDate):
     '''Input the date for search'''
     format='%d/%m/%Y'
     sDate=inDate
+    
     try:
         sDate=datetime.strptime(sDate,format)
     except:
         print("Date is not in desired format [DD-MM-YYYY]")
+    
     day=sDate.day
+    
     monthNum=str(sDate.month)
+    
     datetime_object = datetime.strptime(monthNum, "%m")
+    
     month_name = datetime_object.strftime("%b")
+    
     year=sDate.year
+    
     today=datetime.today()
+    
     if sDate < today:
         print("Selected a future date.")
         contInput()
-    chkDay=day%10
-    if chkDay==1:
+    
+    checkDay=day%10
+    
+    if checkDay==1:
         print("The date entered is {}st day of {} in {}. \n".format(day,month_name,year))
         contInput()
+    
     else:
-        if chkDay==2:
+        if checkDay==2:
             print("The date entered is {}nd day of {} in {}. \n".format(day,month_name,year))
             contInput()
+        
         else:
-            if chkDay==3:
+            if checkDay==3:
                 print("The date entered is {}rd day of {} in {}. \n".format(day,month_name,year))
                 contInput()
-            else:
+        
+        else:
                 print("The date entered is {}th day of {} in {}. \n".format(day,month_name,year))
                 contInput()
-    #day=f"{day:02d}"
-    #monthNum=f"{int(monthNum):02d}"
+    
     return str(day), str(monthNum), str(year)
 
 def contInput():
     '''Check to Continue'''
-    contInput=input("\nPress [c/C] to continue or [q/Q] to exit or [r/R] to reneter the date: ")
+    contInput=input("\nPress [C] to continue or [Q] to exit or [R] to reneter the date: ")
     contIn=str(contInput)
-    if (contIn=='q' or contIn=='Q'):
+    
+    if ( contIn=='Q'):
         exit()
+    
     else:
-        if (contIn=='r' or contIn=='R'):
+        if (  contIn=='R'):
             inputDate()
+        
         else:
-            if (contIn=='c' or contIn=='C'):
+            if ( contIn=='C'):
                 return
+            
             else:
                 try:
                    contInput()
@@ -67,13 +83,13 @@ def contInput():
                     contInput()
 
 def parseURL():
-    tInDate=input("Enter the Traveling Date [DD/MM/YYYY]: ")
+    tInDate=input("Enter the date on which you have to travel [DD/MM/YYYY]: ")
     tInDay,tInMonth,tInYear=inputDate(tInDate)
-    origin=input("Enter the Orgin Airport Code: ")
-    destination=input("Enter the Destination Airport Code: ")
-    adults=input("Enter the number of Adults: ")
-    child=input("Enter the number of Children (age betweem 5 and 18): ")
-    infant=input("Enter the number of Infants: ")
+    airport=input("Enter the code for airport from where you are gonna start the journey: ")
+    destination=input("Enter the code for Destination Airport : ")
+    adult=input("Enter the number of Adults who are going to travel: ")
+    child=input("Enter the number of Children going to travel: ")
+    infant=input("number of Infants: ")
     return origin,destination,tInDate,adults,child,infant
 
 def scrape(url):    
@@ -83,7 +99,6 @@ def scrape(url):
         'Cache-Control': 'no-cache, , max-age=0, must-revalidate, no-store',
         'DNT': '1',
         'Upgrade-Insecure-Requests': '1',
-        # You may want to change the user agent if you get blocked
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
         'Accept-encoding': 'gzip, deflate, br',
@@ -91,19 +106,18 @@ def scrape(url):
         'Referer': 'https://www.makemytrip.com/'
     }
 
-    # Download the page using requests
+    
     print("Downloading %s"%url)
     r = requests.get(url, headers=headers)
     print (r)
-    #r = requests.get(url)
-    # Pass the HTML of the page and create 
+    
     return e.extract(r.text,base_url=url)
 
 
 def main():
     data=parseURL()
     url='https://www.makemytrip.com/flight/search?tripType=1&itinerary='+data[0]+'-'+data[1]+'-'+data[2]+'&paxType=A-'+data[3]+'_C-'+data[4]+'_I-'+data[5]+'&cabinClass=E'
-    #print(urlTemplate.format(paerseURL()))
+    
     with open(__location__+'/data.csv','w') as outfile:
         fieldnames = [
             "name",
