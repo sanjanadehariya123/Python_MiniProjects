6afb2dc7ca5d6ceba486cb8d10a0f63345ab766a
sanjanadehariya123 <91261899+sanjanadehariya123@users.noreply.github.com>
2021-09-27 03:45:37 -0700

    Update long_responses.py

--- chatbot/long_responses.py
+++ chatbot/long_responses.py
@@ -5,9 +5,9 @@ R_ADVICE = "If I were you, I would go to the internet and type exactly what you 
 
 
 def unknown():
-    response = ["Could you please re-phrase that? ",
+    responses = ["Could you please re-phrase that? ",
                 "...",
                 "Sounds about right.",
                 "What does that mean?"][
         random.randrange(4)]
-    return response
+    return responses
